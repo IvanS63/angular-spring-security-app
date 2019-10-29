049c3fcc3091f3fd6af0a39f5892cdfc56e7e87f
Ivan Semenov <semenovivan94@gmail.com>
2019-10-29 23:14:33 +0400

    -add user module

---
+++ room-booking-app-frontend/src/app/users/user.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from './user';
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+    
+}
