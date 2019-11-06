d79856b633f1056fb0486213e5a5c8a05f709fb3
Ivan Semenov <ivan_semenov@epam.com>
2019-11-06 14:14:15 +0400

    -add redirecting to /login for 401-response

--- room-booking-app-frontend/src/app/auth/auth.interceptor.ts
+++ room-booking-app-frontend/src/app/auth/auth.interceptor.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { TokenStorageService } from './token-storage.service';
+import { Router } from '@angular/router';
+import {tap} from 'rxjs/internal/operators';
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private token: TokenStorageService) { }
+    constructor(private token: TokenStorageService, private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         let authReq = req;
@@ -17,7 +19,16 @@ export class AuthInterceptor implements HttpInterceptor {
                 headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token)
             });
         }
-        return next.handle(authReq);
+        return next.handle(authReq).pipe(tap((event: HttpEvent<any>) => {
+            if (event instanceof HttpResponse) {
+            }
+          }, (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+              if (err.status === 401) {
+                  this.router.navigate(['login']);
+              }
+            }
+          }));
     }
 }
 
