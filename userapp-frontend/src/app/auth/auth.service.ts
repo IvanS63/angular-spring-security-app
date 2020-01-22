040e9615a30b350256001ccc6c3e2ac348cb034f
Ivan Semenov <ivan_semenov@epam.com>
2020-01-22 11:56:03 +0400

    refactored code style

--- userapp-frontend/src/app/auth/auth.service.ts
+++ userapp-frontend/src/app/auth/auth.service.ts
@@ -5,9 +5,11 @@ import { LoginDto } from './login-dto';
 import { JwtResponseDto } from './jwt-response-dto';
 import { SignupDto } from './sign-up-dto';
 
+const lOGIN_URL = 'http://localhost:8090/userapp-backend/auth/login';
+const SIGNUP_URL = 'http://localhost:8090/userapp-backend/auth/signup';
 
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json'}),
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     withCredentials: true
 };
 
@@ -15,18 +17,15 @@ const httpOptions = {
     providedIn: 'root'
 })
 export class AuthService {
-    private loginUrl = 'http://localhost:8090/userapp-backend/auth/login';
-    private signuoUrl = 'http://localhost:8090/userapp-backend/auth/signup';
-
     constructor(private http: HttpClient) {
     }
 
     authenticate(credentials: LoginDto): Observable<JwtResponseDto> {
-        return this.http.post<JwtResponseDto>(this.loginUrl, credentials, httpOptions);
+        return this.http.post<JwtResponseDto>(lOGIN_URL, credentials, httpOptions);
     }
 
     signUp(info: SignupDto): Observable<any> {
-        return this.http.post<any>(this.signuoUrl, info, httpOptions);
+        return this.http.post<any>(SIGNUP_URL, info, httpOptions);
     }
 
 }
