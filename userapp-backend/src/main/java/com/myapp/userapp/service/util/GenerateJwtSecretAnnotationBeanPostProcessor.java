07abda5fd71b208eb2058d92534e7792c2e28086
Ivan Semenov <ivan_semenov@epam.com>
2020-02-05 15:01:47 +0400

    done refactoring and naming conventions

--- userapp-backend/src/main/java/com/myapp/userapp/service/util/GenerateJwtSecretAnnotationBeanPostProcessor.java
+++ userapp-backend/src/main/java/com/myapp/userapp/service/util/GenerateJwtSecretAnnotationBeanPostProcessor.java
@@ -2,9 +2,9 @@ package com.myapp.userapp.service.util;
 
 import com.myapp.userapp.service.security.JwtProvider;
 import com.myapp.userapp.util.GenerateJwtSecret;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -16,35 +16,32 @@ import java.util.Random;
  * @author Ivan_Semenov
  */
 @Component
-public class JwtSecretInitializer implements BeanPostProcessor {
+public class GenerateJwtSecretAnnotationBeanPostProcessor implements BeanPostProcessor {
+    
     private Random random = new Random();
 
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+    public Object postProcessBeforeInitialization(Object bean, String beanName)  {
         Field[] fields = bean.getClass().getDeclaredFields();
         for (Field field : fields) {
             GenerateJwtSecret annotation = field.getAnnotation(GenerateJwtSecret.class);
             if (annotation != null) {
                 if (!field.getType().equals(String.class))
-                    throw new RuntimeException("@GenerateJwtSecret cannot be set above " + field.getType());
+                    throw new IllegalStateException("@GenerateJwtSecret cannot be set above " + field.getType());
                 if (Modifier.isFinal(field.getModifiers())) {
-                    throw new RuntimeException("Unable to inject to final fields");
+                    throw new IllegalStateException("Unable to inject to final fields");
                 }
                 String[] values = annotation.values();
                 String value = values[random.nextInt(values.length)];
-                try {
-                    field.setAccessible(true);
-                    field.set(bean, value);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, bean, value);
             }
         }
         return bean;
     }
 
     @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
         return bean;
     }
 }
